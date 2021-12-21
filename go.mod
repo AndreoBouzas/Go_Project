9e33de79956d7f9193d948d77c3ae12c02668858
Andreo <andreobouzas@gmail.com>
2021-12-21 17:25:52 -0300

    Base Go Web Crawler Completa

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/AndreoBouzas/Go_Project
 
 go 1.13
+
+require (
+	github.com/steelx/extractlinks v0.0.2
+	golang.org/x/net v0.0.0-20211216030914-fe4d6282115f // indirect
+)
--- goWebCrawler/gowebCrawler.go
+++ goWebCrawler/gowebCrawler.go
@@ -1,19 +1,105 @@
-package Webcrawler
+package goWebcrawler
 
 import (
+	"crypto/tls"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
+
+	"github.com/steelx/extractlinks"
+)
+
+var (
+	config = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	transport = &http.Transport{
+		TLSClientConfig: config,
+	}
+	NetClient = &http.Client{
+		Transport: transport,
+	}
+	queue = make(chan string)
+
+	hasVisited = make(map[string]bool)
 )
 
 func Webcrawler() {
-	baseURL := "http://youtube.com/jsfunc"
-	responsehttp, err := http.Get(baseURL)
-	checkErr(err)
+	arguments := os.Args[1:]
 
-	fmt.Println(responsehttp)
+	if len(arguments) == 0 {
+		fmt.Println("Missing URL, e.g. go-webscrapper http://js.org/")
+		os.Exit(1)
+	}
+
+	baseURL := arguments[0]
+	go func() {
+		queue <- baseURL
+	}()
+
+	for href := range queue {
+		if !hasVisited[href] && isSameDomain(href, baseURL) {
+			crawlUrl(href)
+		}
+
+	}
 
 }
+
+func crawlUrl(href string) {
+	hasVisited[href] = true
+	fmt.Printf("Crawling url -> %v \n ", href)
+	responsehttp, err := NetClient.Get(href)
+	checkErr(err)
+	defer responsehttp.Body.Close()
+
+	links, err := extractlinks.All(responsehttp.Body)
+	checkErr(err)
+
+	for _, link := range links {
+		absoluteURL := toFixedURL(link.Href, href)
+		go func() {
+			queue <- absoluteURL
+		}()
+
+	}
+
+	responsehttp.Body.Close()
+
+}
+
+func isSameDomain(href, baseUrl string) bool {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return false
+	}
+	parentUri, err := url.Parse(baseUrl)
+	if err != nil {
+		return false
+	}
+	if uri.Host != parentUri.Host {
+		return false
+	}
+	return true
+}
+
+func toFixedURL(href, baseURL string) string {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+
+	base, err := url.Parse(baseURL)
+	if err != nil {
+		return ""
+	}
+
+	toFixedUri := base.ResolveReference(uri)
+
+	return toFixedUri.String()
+}
+
 func checkErr(err error) {
 	if err != nil {
 		fmt.Println("erro", err)
